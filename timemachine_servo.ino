fb6a74e2bd28a3d55822ababd8f02cf0390fc8de
Lisa Pothoven <lisa.pothoven@xs4all.nl>
2018-03-17 17:02:08 +0100

    First version

---
+++ timemachine_servo.ino
@@ -0,0 +1,72 @@
+#include <EasyTransfer.h>
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+//create object
+EasyTransfer ET; 
+
+struct RECEIVE_DATA_STRUCTURE{
+  //put your variable definitions here for the data you want to receive
+  //THIS MUST BE EXACTLY THE SAME ON THE OTHER ARDUINO
+  int16_t lengthID; // An ID number 0, 1, 2, 3, 4
+};
+
+//give a name to the group of data
+RECEIVE_DATA_STRUCTURE mydata;
+
+int pos = 0;    // variable to store the servo position
+float P = 0;
+//float g = 9.8 // gravitational acceleration
+float len = 0; // length of the pendulum -- from servo turning point to center of ball.
+
+void setup() {
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+
+  Serial.begin(9600);
+  ET.begin(details(mydata), &Serial);
+}
+
+float period(float L) {
+  L = L/100.0; //length in m
+  P = 2*PI*sqrt(L/9.8);
+  return P*1000; //period in ms
+}
+
+void loop() {
+  //check and see if a data packet has come in. 
+  if(ET.receiveData()){
+    //this is how you access the variables. [name of the group].[variable name]
+    switch (mydata.lengthID) {
+      case 0:
+        P = period(10); // cm
+        break;
+      case 1:
+        P = period(13); // cm
+        break;
+      case 2:
+        P = period(16); // cm
+        break;
+      case 3:
+        P = period(19); // cm
+        break;
+      case 4:
+        P = period(22); // cm
+        break;
+      case 5:
+        P = period(25); 
+        break;
+    }    
+    Serial.println(P);
+  }
+  for (pos = 50; pos <= 130; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(P/80);                       // waits for the servo to reach the position
+  }
+  for (pos = 130; pos >= 50; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(P/80);                       // waits for the servo to reach the position
+  }
+}
---
+++ timemachine_stepper.ino
@@ -0,0 +1,70 @@
+#include <EasyTransfer.h>
+#include <X113647Stepper.h>
+
+//create object
+EasyTransfer ET; 
+
+struct SEND_DATA_STRUCTURE{
+  //put your variable definitions here for the data you want to send
+  int16_t lengthID; // An ID number 0, 1, 2, 3, 4
+};
+
+//give a name to the group of data
+SEND_DATA_STRUCTURE mydata;
+
+static const int STEPS_PER_REVOLUTION = 64 * 32;  // the number of steps per revolution for the motor
+
+// initialize the stepper library on pins 2 through 5:
+X113647Stepper myStepper(STEPS_PER_REVOLUTION, 2, 3, 4, 5);
+
+int motorspeed = 5.5;
+
+int sensorPin = A0;
+int sensorValue = 0;
+
+int previousID;
+
+void setup() {
+  // set the speed in rpm:
+  Serial.begin(9600);
+  myStepper.setSpeed(motorspeed);  
+  ET.begin(details(mydata), &Serial);
+  previousID = 0;
+}
+
+void loop() {
+  sensorValue = analogRead(sensorPin);
+  if (sensorValue < 190){ // 250 cm
+    mydata.lengthID = 4;
+  }
+  if (sensorValue < 152){ // 200 cm
+    mydata.lengthID = 3;
+  }
+  if (sensorValue < 114 ){ // 150 cm
+    mydata.lengthID = 2;
+  }
+  if (sensorValue < 76 ){ // 100 cm
+    mydata.lengthID = 1;
+  }
+  if (sensorValue < 39 ){ // 50 cm
+    mydata.lengthID = 0;
+  }
+  else if (sensorValue > 190) {
+    mydata.lengthID = 5;
+  }
+
+  if (previousID != mydata.lengthID) { // only if the value has 
+    ET.sendData(); // send to arduino buddy
+    int ID = previousID;
+    previousID = mydata.lengthID; 
+    while (ID > mydata.lengthID) {
+      myStepper.step(STEPS_PER_REVOLUTION / 2); // reel in
+      ID--;
+    } 
+    while (ID < mydata.lengthID) {
+      myStepper.step(-STEPS_PER_REVOLUTION / 2); // reel out
+      ID++;
+    }
+  }
+  delay(2000); // wait for a bit
+}
